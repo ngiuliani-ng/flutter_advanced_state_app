252c81d8eaedad851646ded1387dd62d0cd8f2b7
Nicolò Giuliani <info@ngiuliani.com>
2021-04-07 06:42:57 +0200

    Basic checkout page definition.

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -5,6 +5,25 @@ class CheckoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: appBar(context),
+      body: body(context),
+    );
+  }
+
+  Widget appBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      elevation: 0,
+    );
+  }
+
+  Widget body(BuildContext context) {
+    return Container();
   }
 }
